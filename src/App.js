391d7c1d06276736b42624ca73a40da9130f0be0
bhandariamey <bhandariamey@gmail.com>
2024-01-19 14:08:39 +0530

    pushing some images coming from aws s3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Mantra/1.jpg"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Mantra/10.jpg"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Mantra/2.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Mantra/6.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Mantra/7.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Mantra/8.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Mantra/4.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Dreamroom/1.JPG"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Dreamroom/2.JPG"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://s3.ap-south-1.amazonaws.com/thedesignspell.netlify.app/photos/sites/Dreamroom/3.JPG"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+
+    </Carousel>
   );
 }
 
